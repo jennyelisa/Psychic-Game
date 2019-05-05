b6d7e63f333a534e95e1d6d14860dfcbde33865c
Jenny <jennyelisa07@aol.com>
2019-05-04 20:52:00 -0400

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,37 +6,40 @@ var guessesRemainingCounter = 8;
 var guessedLetter = [];
 var correctLetter = [];
 var computerGuess=""
+var outcome = "";
 
 function reset() {
     guessesRemainingCounter = 8;
     userGuess = "";
     computerGuess = letterPool[Math.floor(Math.random() * letterPool.length)];
-    
     console.log(computerGuess)
 }
+
 
 reset();
 
 //once computer has its guess the user can choose their letter
 //key up command
+
 document.onkeyup = function (event) {
     var userGuess = event.key;
     console.log(userGuess);
-
+    // userGuess.push(guessedLetter);
     //users guess matches computer then win count goes up. game should restart
     if (userGuess === computerGuess) {
         alert('You Got IT!');
         winsCounter++;
+        document.getElementById('outcome').textContent = "You WON! :D";
         reset();
-    } else { // tell player to try again, game resets and loss count should go up
+    } else { // tell player to try again, guesses go down by 1
         alert('TRY again!');
         guessesRemainingCounter--;
         
-        // document.getElementById("guessesRemainingCounter").innerHTML = guessesRemainingCounter--;
         if (guessesRemainingCounter === 0) {
             alert('Game Over!');
             lossesCounter++;
             guessesRemainingCounter--;
+            document.getElementById('outcome').textContent = "Out of tries! D:";
             reset();
         }
     };
@@ -48,10 +51,9 @@ document.onkeyup = function (event) {
     document.getElementById('wins').textContent = "You have " + winsCounter + " win! :D";
     document.getElementById('losses').textContent = "You have  " + lossesCounter + " loss! D:";
     document.getElementById('guessesRemaining').textContent = "You have " + guessesRemainingCounter + " guesses left";
-    // document.getElementById('correct-letter').textContent = "The letter was : " + computerGuess;
+    document.getElementById('correct-letter').textContent = "The letter was : " + computerGuess;
     //line about shows the guessed letter how to make this appear after out of tries
-    document.getElementById('outcome').textContent = "You WON! :D";
-    document.getElementById('outcome').textContent = "You LOST! D:";
+    
 
 
 };
